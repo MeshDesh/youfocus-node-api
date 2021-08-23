87a2616c244821d62aedbdc0407bbe2e1ee5006a
Shashank Bhat <shashank.bhat2525@gmail.com>
2021-08-23 14:15:17 +0530

    fix: formatted the code

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -1,23 +1,27 @@
-import configEnv from "../config";
-import { Playlist, PlaylistData, PlaylistInfo, VideoModel } from "../interfaces";
-import { google } from "googleapis";
+import configEnv from '../config';
+import {
+  Playlist,
+  PlaylistData,
+  PlaylistInfo,
+  VideoModel,
+} from '../interfaces';
+import { google } from 'googleapis';
 
 //youtube api config;
 const youtube = google.youtube({
   version: 'v3',
-  auth: configEnv.YOUTUBE_API_KEY
+  auth: configEnv.YOUTUBE_API_KEY,
 });
 
 //extracting playlist id;
 export const getPlaylistId = (url: string) => {
-  var reg = new RegExp("[&?]list=([a-z0-9_]+)", "i");
+  var reg = new RegExp('[&?]list=([a-z0-9_]+)', 'i');
   var match = reg.exec(url);
 
   if (match && match[1].length > 0) {
     return match[1];
   }
-
-}
+};
 
 const getPlaylistInfo = async (playlistId: string) => {
   return await youtube.playlists
@@ -42,53 +46,61 @@ const getPlaylistInfo = async (playlistId: string) => {
     });
 };
 
-
 const getPlaylistData = async (playlistId: string, pageToken: string) => {
-  return await youtube.playlistItems.list({
-    playlistId,
-    part: ['snippet', 'id'],
-    pageToken
-  }).then(async(response) => {
+  return await youtube.playlistItems
+    .list({
+      playlistId,
+      part: ['snippet', 'id'],
+      pageToken,
+    })
+    .then(async response => {
+      const { pageInfo, prevPageToken, nextPageToken, items } = response.data;
 
-    const { pageInfo, prevPageToken, nextPageToken, items } = response.data;
-
-    return {
-      playlistMeta: pageInfo,
-      nextPageToken: nextPageToken,
-      prevPageToken: prevPageToken,
-      videos: items?.map((video) => {
-        const { snippet } = video;
-        let videoData = {
-          id: snippet?.resourceId?.videoId,
-          title: snippet?.title,
-          thumbnail: snippet?.thumbnails?.default?.url
-        } as VideoModel
-        return  videoData
-      }),
-      ...response.data
-    } as PlaylistData
-  }).catch((error) => {
-    return {
-      message: 'There was an error',
-      error
-    }
-  })
-}
-
+      return {
+        playlistMeta: pageInfo,
+        nextPageToken: nextPageToken,
+        prevPageToken: prevPageToken,
+        videos: items?.map(video => {
+          const { snippet } = video;
+          let videoData = {
+            id: snippet?.resourceId?.videoId,
+            title: snippet?.title,
+            thumbnail: snippet?.thumbnails?.default?.url,
+          } as VideoModel;
+          return videoData;
+        }),
+        ...response.data,
+      } as PlaylistData;
+    })
+    .catch(error => {
+      return {
+        message: 'There was an error',
+        error,
+      };
+    });
+};
 
 //Get playlist items
-export const getPlaylist = async (playlistId: string, pageToken: string) => {  
+export const getPlaylist = async (playlistId: string, pageToken: string) => {
+ 
   let playlist = {} as Playlist;
-  try{
-    const playlistInfo = await getPlaylistInfo(playlistId) as PlaylistInfo;
-    const playlistData = await getPlaylistData(playlistId, pageToken) as PlaylistData
+  
+  try {
+
+    const playlistInfo = (await getPlaylistInfo(playlistId)) as PlaylistInfo;
+    
+    const playlistData = (await getPlaylistData(
+      playlistId,
+      pageToken,
+    )) as PlaylistData;
+
     playlist = {
       playlistInfo,
-      playlistData
-    }
+      playlistData,
+    };
+    
     return playlist;
-  }catch(error){
+  } catch (error) {
     console.log(error.message);
   }
-}
-
+};
